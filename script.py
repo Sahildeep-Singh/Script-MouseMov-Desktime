a8636a001cd9247d44ee03068046ecd4be8544c8
Sahildeep Singh <420sahildeepsingh@gmail.com>
2023-03-05 15:19:14 +0530

    changes

--- script.py
+++ script.py
@@ -1,18 +1,19 @@
-
 import pyautogui
 from numpy import random
 
-q, s, t = 'l', int(input())*150, 'f'
+q, e, s, l, t = 'l', 1919, int(input())*150, 103, 'f'
 
-m, xC, u, yC, v = 't', [], '4', [], 'a'
+m, xC, u, c, yC, v = 't', [], '4', 1033, [], 'a'
 
 def f():
   x,y = pyautogui.position()
-  return True if x != 1919 else False
+  return True if x != e else False
+
+pyautogui.moveTo(e, l)  
 
 for y in range(s):
-  yC.append(random.randint(103, 1033))
-  xC.append(1919)
+  yC.append(random.randint(l, c))
+  xC.append(e)
 
 
 for o in range(s):
@@ -21,5 +22,6 @@ for o in range(s):
   if(f()): break
 
 print("※")
-pyautogui.hotkey(v + q + m , t + u)
+# pyautogui.hotkey(v + q + m , t + u)py sc  100
+
 
